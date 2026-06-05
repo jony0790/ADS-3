dc66690155369976763bbe0e420cbffe709ffc8c
Джалоладдин <joniatabekovich07@gmail.com>
2026-06-05 18:43:17 +0300

    Джумабаев Джалоладдин 25-ИСТ-1-1

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,15 +1,16 @@
 #include <cstdint>
-
+#include "alg.h"
+// Collatz ketma-ketligi uzunligi
 unsigned int collatzLen(uint64_t num)
 {
     unsigned int len = 1;
 
     while (num != 1)
     {
-        if (num % 2 == 0)
-            num /= 2;
-        else
+        if (num & 1)
             num = 3 * num + 1;
+        else
+            num /= 2;
 
         ++len;
     }
@@ -17,16 +18,17 @@ unsigned int collatzLen(uint64_t num)
     return len;
 }
 
+// Collatz ketma-ketligidagi maksimal qiymat
 uint64_t collatzMaxValue(uint64_t num)
 {
     uint64_t maxValue = num;
 
     while (num != 1)
     {
-        if (num % 2 == 0)
-            num /= 2;
-        else
+        if (num & 1)
             num = 3 * num + 1;
+        else
+            num /= 2;
 
         if (num > maxValue)
             maxValue = num;
@@ -35,6 +37,7 @@ uint64_t collatzMaxValue(uint64_t num)
     return maxValue;
 }
 
+// [lbound, rbound] oralig'ida eng uzun ketma-ketlik beruvchi son
 unsigned int seqCollatz(unsigned int *maxlen,
                         uint64_t lbound,
                         uint64_t rbound)
